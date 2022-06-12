4ee24807263483c6b82417a764a31851ef2b41ff
Hoca Chen <hocachen@gmail.com>
2022-06-12 10:14:33 +0800

    add file

---
+++ PI_Motion/PI_Motion/PIMotion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PI_Motion
+{
+    public class PIMotion
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
